7c5c6ccceb86861c76ba4e9a63199942164bf0d9
sirbobisherenow <chetan.likes.nature@gmail.com>
2026-07-11 09:24:25 -0700

    Started working on firmware

---
+++ firmware/INFO.md
@@ -0,0 +1,11 @@
+# Info
+Pins and what they do. Used as reference when programming firmware. 
+
+| **Component** | **Function(connected to)** | **Pin(s)** |
+| --- | ---  | --- |
+| Matrix Column | Columns 1, 2, & 3 | D0-2 |
+| Matrix Row | Row  1, 2, & 3 | D3, D10, D9 |
+| Encoder Button | Pin | D6 |
+| Encoder axis | Pin A & B | D8, D7 |
+
+**WHEN INSTALLING ADD KMK FOLDER!**
---
+++ firmware/main.py
@@ -0,0 +1,55 @@
+# Import modules needed
+import board
+from kmk.kmk_keyboard import KMKKeyboard
+from kmk.keys import KC
+from kmk.scanners import DiodeOrientation
+from kmk.scanners.keypad import MatrixScanner, KeysScanner
+from kmk.modules.encoder import EncoderHandler
+from kmk.extensions.media_keys import MediaKeys
+
+gooberPad = KMKKeyboard()
+
+# media keys(vol, skipping pausing)
+gooberPad.extensions.append(MediaKeys())
+
+#Create the 3x3 matrix
+keyMatrix = MatrixScanner(
+    column_pins=[board.D0, board.D1, board.D2],
+    row_pins=[board.D3, board.D10, board.D9],
+    diode_orientation=DiodeOrientation.COL2ROW # this might be wrong, idk
+)
+# encoder button setup
+encoderButton = KeysScanner(
+    pins = [board.D6],
+    value_when_pressed = False,
+    pull=True
+)
+
+# hook up keymatrix and the encoder button to the gooberPad
+gooberPad.matrix = [keyMatrix, encoderButton]
+
+encoder = EncoderHandler()
+# adding the encoder pins to encoder. Using none as last argument since the encoder's button is already assigned
+encoder.pins(
+    (board.D8, board.D7, None),
+)
+# add the encoder to the gooberPad
+gooberPad.modules.append(encoder)
+
+#Assign basic functions
+gooberPad.keymap = [
+    [
+        # Row 1
+        KC.MEDIA_PREV_TRACK, KC.MEDIA_PLAY_PAUSE, KC.MEDIA_NEXT_TRACK,
+        # Row 2     
+        # Row 3
+        # Encoder button
+    ]
+]
+encoder.map = [
+    # Layer 0
+    ((KC.VOLD, KC.VOLU),) 
+]
+
+if __name__ == '__main__':
+    gooberPad.go()
